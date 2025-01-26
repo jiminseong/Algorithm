672a5eb72999d614723db53e304053b0efe4d8d3
jiminseong <iamjms4237@gachon.ac.kr>
2025-01-26 18:08:23 +0900

    Programmers:더맵게

--- Programmers/week3/더 맵게.js
+++ Programmers/week3/더 맵게.js
@@ -1,18 +1,81 @@
-// 문제 설명
-// 매운 것을 좋아하는 Leo는 모든 음식의 스코빌 지수를 K 이상으로 만들고 싶습니다.
-// 모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 Leo는 스코빌 지수가 가장 낮은 두 개의 음식을 아래와 같이 특별한 방법으로 섞어 새로운 음식을 만듭니다.
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
 
-// 섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
-// Leo는 모든 음식의 스코빌 지수가 K 이상이 될 때까지 반복하여 섞습니다.
-// Leo가 가진 음식의 스코빌 지수를 담은 배열 scoville과 원하는 스코빌 지수 K가 주어질 때,
-// 모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 섞어야 하는 최소 횟수를 return 하도록 solution 함수를 작성해주세요.
+  getParentIndex(index) {
+    return Math.floor((index - 1) / 2);
+  }
 
-// 제한 사항
-// scoville의 길이는 2 이상 1,000,000 이하입니다.
-// K는 0 이상 1,000,000,000 이하입니다.
-// scoville의 원소는 각각 0 이상 1,000,000 이하입니다.
-// 모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우에는 -1을 return 합니다.
+  getLeftChildIndex(index) {
+    return 2 * index + 1;
+  }
 
-// 입출력 예
-// scoville	K	return
-// [1, 2, 3, 9, 10, 12]	7	2
+  getRightChildIndex(index) {
+    return 2 * index + 2;
+  }
+
+  swap(index1, index2) {
+    [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]];
+  }
+
+  insert(value) {
+    this.heap.push(value);
+    this.heapifyUp();
+  }
+
+  heapifyUp() {
+    let index = this.heap.length - 1;
+    while (index > 0 && this.heap[index] < this.heap[this.getParentIndex(index)]) {
+      this.swap(index, this.getParentIndex(index));
+      index = this.getParentIndex(index);
+    }
+  }
+
+  remove() {
+    if (this.heap.length === 0) return null;
+    if (this.heap.length === 1) return this.heap.pop();
+
+    const root = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.heapifyDown();
+    return root;
+  }
+
+  heapifyDown() {
+    let index = 0;
+    while (this.getLeftChildIndex(index) < this.heap.length) {
+      let smallerChildIndex = this.getLeftChildIndex(index);
+      if (
+        this.getRightChildIndex(index) < this.heap.length &&
+        this.heap[this.getRightChildIndex(index)] < this.heap[smallerChildIndex]
+      ) {
+        smallerChildIndex = this.getRightChildIndex(index);
+      }
+
+      if (this.heap[index] <= this.heap[smallerChildIndex]) break;
+
+      this.swap(index, smallerChildIndex);
+      index = smallerChildIndex;
+    }
+  }
+}
+
+function solution(scoville, K) {
+  let count = 0;
+  const scovileleHeap = new MinHeap();
+
+  scoville.forEach((element) => {
+    scovileleHeap.insert(element);
+  });
+
+  while (scovileleHeap.heap.length > 1 && scovileleHeap.heap[0] < K) {
+    const min1 = scovileleHeap.remove();
+    const min2 = scovileleHeap.remove();
+    const newScoville = min1 + min2 * 2;
+    scovileleHeap.insert(newScoville);
+    count++;
+  }
+
+  return scovileleHeap.heap[0] >= K ? count : -1;
+}
